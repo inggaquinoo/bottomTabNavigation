35fe172e73543fc25207cfc140363af4513a966c
Gustavo Aquino <inggaquinoo@gmail.com>
2022-06-13 17:51:16 -0500

    modified text in app

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hi, Im Gustavo, Im here now</Text>
+      <Text>Hi, Im Gustavo, Im here now---again!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
